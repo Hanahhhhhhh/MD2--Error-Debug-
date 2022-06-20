ffcf3413bbc400ac7e0c2c92a3bc041d43ac7788
hanahhhhhhhh <hanguyen18052004@gmail.com>
2022-06-20 21:54:48 +0700

    first commit

---
+++ BT-debug/main.ts
@@ -0,0 +1,25 @@
+function calculator(number1, number2, operator) {
+    let result;
+    if (operator == '+') {
+        result = number1 + number2;
+    } else if (operator == '-') {
+        result = number1 - number2;
+    } else if (operator == '*') {
+        result = number1 * number2;
+    } else if (operator == '/') {
+        result = number1 / number2;
+    } else {
+        throw RangeError(`input ${operator}  is not expected \n the operator must be an +,-,*,/`)
+    }
+
+    return result;
+}
+
+try{
+    calculator(3, 9, '%')
+}
+catch (error){
+    if (error instanceof RangeError){
+        console.log(error.message)
+    }
+}
